881512ea7e75c1bcd156c71db6a946b30ea7886a
Hong-Kuan Wu <whk0313@gmail.com>
2019-05-23 13:20:47 +0800

    fix bug because branch conflict (#16)

--- src/quiThink.js
+++ src/quiThink.js
@@ -238,7 +238,7 @@ class QuiThink {
     } else {
       // close
       this.clear()
-      game.addScore(section[QUI.level], this.currentScore) // 一個向度結束後更新遊戲總分
+      game.addScore(this.currentScore) // 一個向度結束後更新遊戲總分
       game.currentScore = this.currentScore
       game.over()
     }
